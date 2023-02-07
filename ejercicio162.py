5659794dc272ed716d69568643534d5d6f9dc5a5
David Pérez <knightbcn@gmail.com>
2023-02-07 12:48:41 +0100

    Exercise162 added docstring

--- ejercicio162.py
+++ ejercicio162.py
@@ -5,5 +5,13 @@ Using the math built-in module, implement the function called sigmoid()
 import math
 
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+def sigmoid(var_x):
+    """_summary_
+
+    Args:
+        var_x (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return 1 / (1 + math.exp(-var_x))
